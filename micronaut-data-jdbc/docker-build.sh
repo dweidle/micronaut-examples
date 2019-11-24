70a8fd5aca77b555fb99522883f3fd78af61b3b4
Daniel Weidle <dw@danielweidle.de>
2019-11-24 15:42:26 +0100

    Tag container image

--- micronaut-data-jdbc/docker-build.sh
+++ micronaut-data-jdbc/docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker build . -t micronaut-data-jdbc
+docker build . -t dweidle/micronaut-data-jdbc
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run -p 8080:8080 micronaut-data-jdbc"
+echo "    $ docker run -p 8080:8080 dweidle/micronaut-data-jdbc"
